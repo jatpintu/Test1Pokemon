30dfc884d082126767a3643f73d60d5591bcb173
Pintu jat <pintu.chaudhary9@gmail.com>
2019-10-31 12:00:58 -0400

    making pokemon selected and getting the correct pokemon selected in watch console and making textfield in watch and getting the name

--- CommunicationTest WatchKit Extension/InterfaceController.swift
+++ CommunicationTest WatchKit Extension/InterfaceController.swift
@@ -10,19 +10,22 @@ import WatchKit
 import Foundation
 import WatchConnectivity
 
+@available(watchOSApplicationExtension 6.0, *)
 class InterfaceController: WKInterfaceController, WCSessionDelegate {
 
-    
-    // MARK: Outlets
-    // ---------------------
+
     @IBOutlet var messageLabel: WKInterfaceLabel!
-   
-    // Imageview for the pokemon
+
     @IBOutlet var pokemonImageView: WKInterfaceImage!
-    // Label for Pokemon name (Albert is hungry)
+
     @IBOutlet var nameLabel: WKInterfaceLabel!
-    // Label for other messages (HP:100, Hunger:0)
+
     @IBOutlet var outputLabel: WKInterfaceLabel!
+    @IBOutlet var giveNameLabel: WKInterfaceLabel!
+    @IBOutlet var textField: WKInterfaceTextField!
+    
+    var pokemonName : String = ""
+    var pokemonSelected : String = ""
     
     
     
@@ -38,20 +41,17 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
     func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
         print("WATCH: Got message from Phone")
         // Message from phone comes in this format: ["course":"MADT"]
-        let messageBody = message["course"] as! String
-        messageLabel.setText(messageBody)
+//        let messageBody = message["course"] as! String
+        self.pokemonSelected = message["pokemon"] as! String
+       
+        print(self.pokemonSelected)
+        
     }
-    
-
-
-    
+   
     // MARK: WatchKit Interface Controller Functions
-    // ----------------------------------
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        
-        // 1. Check if teh watch supports sessions
+
         if WCSession.isSupported() {
             WCSession.default.delegate = self
             WCSession.default.activate()
@@ -61,22 +61,22 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
     }
     
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
         super.willActivate()
         
-        
+        self.giveNameLabel.setHidden(true)
+        self.textField.setHidden(true)
     }
     
     override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
 
+    @IBAction func namePokemon(_ value: NSString?) {
+        self.pokemonName = value! as String
+        self.giveNameLabel.setText(pokemonName)
+    }
     
-    // MARK: Actions
-    // ---------------------
-    
-    // 2. When person presses button on watch, send a message to the phone
+    //send a message to the phone
     @IBAction func buttonPressed() {
         
         if WCSession.default.isReachable {
@@ -86,11 +86,9 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
                 ["name" : "Pritesh"],
                 replyHandler: {
                     (_ replyMessage: [String: Any]) in
-                    // @TODO: Put some stuff in here to handle any responses from the PHONE
                     print("Message sent, put something here if u are expecting a reply from the phone")
                     self.messageLabel.setText("Got reply from phone")
             }, errorHandler: { (error) in
-                //@TODO: What do if you get an error
                 print("Error while sending message: \(error)")
                 self.messageLabel.setText("Error sending message")
             })
