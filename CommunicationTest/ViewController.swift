8e2c6bc6afb8df68b0bc7ff9e2c0a66cff7df0f9
Pintu jat <pintu.chaudhary9@gmail.com>
2019-10-31 17:55:07 -0400

    pokemon health and hunger updating according to its hunger level

--- CommunicationTest/ViewController.swift
+++ CommunicationTest/ViewController.swift
@@ -23,18 +23,23 @@ class ViewController: UIViewController, WCSessionDelegate  {
     @IBOutlet weak var healthpok2: UILabel!
     
     
-    var seconds = 100;
+    var seconds = 0;
     var startTime = 100;
     var currennttime = 0;
     var timeDuration = 0;
     var timer = Timer();
+    var hungerTimerpok1 = Timer();
+    var hungerTimerpok2 = Timer();
     var hungerpok1value = 0;
     var hungerpok2value = 0;
     var healthpok1value = 100;
     var healthpok2value = 100;
+    
+    
+    
     @objc func timerAction() {
-        if(self.seconds>0){
-        self.seconds -= 1
+        if(self.seconds<200){
+        self.seconds += 1
         timeLabel.text = "\(self.seconds)"
         }
       }
@@ -68,11 +73,11 @@ class ViewController: UIViewController, WCSessionDelegate  {
         if(self.pokSelected == self.pok1){
             self.pokemon1.alpha = 1.0
             self.pokemon1.text = self.pokNameWatch
-        playGame();
+//        playGame();
         }else{
             self.pokemon2.alpha = 1.0
             self.pokemon2.text = self.pokNameWatch
-        playGame();
+//        playGame();
         }
         
     }
@@ -118,7 +123,9 @@ class ViewController: UIViewController, WCSessionDelegate  {
                else {
                    print("PHONE: Cannot reach watch")
                }
-        timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+        hungerTimerpok1 = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(hungerAction1), userInfo: nil, repeats: true)
+        hungerTimerpok2 = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(hungerAction), userInfo: nil, repeats: true)
    
     }
     @IBAction func caterpieButtonPressed(_ sender: Any) {
@@ -126,6 +133,9 @@ class ViewController: UIViewController, WCSessionDelegate  {
 //        self.pokNameWatch = "caterpie"
         self.selectlabel.alpha = 0.0
          timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+        hungerTimerpok1 = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(hungerAction1), userInfo: nil, repeats: true)
+        hungerTimerpok2 = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(hungerAction), userInfo: nil, repeats: true)
+        
         if (WCSession.default.isReachable) {
             let message = ["pokemon": "pok2"] as [String : Any]
                    WCSession.default.sendMessage(message, replyHandler: nil)
@@ -134,17 +144,41 @@ class ViewController: UIViewController, WCSessionDelegate  {
                else {
                    print("PHONE: Cannot reach watch")
                }
-    }
-
-    
-    
-    public func playGame(){
-        self.hungrypok1.text = "\(self.hungerpok1value)"
-        self.healthpok1.text = "\(self.healthpok1value)"
-                   self.healthpok2.text = "\(self.healthpok2value)"
-                   self.hungrypok2.text = "\(self.hungerpok2value)"
         
     }
+
+    @objc func hungerAction1() {
+      if(self.hungerpok1value<100){
+      self.hungerpok1value += 10
+      hungrypok1.text = "\(self.hungerpok1value)"
+      healthpok1.text = "\(self.healthpok1value)"
+        if(self.hungerpok1value > 80){
+            self.healthpok1value -= 5
+            self.healthpok1.text = "\(self.healthpok1value)"
+         }
+      }
+    }
+    @objc func hungerAction() {
+      if(self.hungerpok2value<100){
+      self.hungerpok2value += 10
+      hungrypok2.text = "\(self.hungerpok2value)"
+      healthpok2.text = "\(self.healthpok2value)"
+        if(self.hungerpok2value > 80){
+            self.healthpok2value -= 5
+            self.healthpok2.text = "\(self.healthpok2value)"
+        }
+      }
+    }
+    
+//    public func playGame(){
+//
+////        self.hungrypok1.text = "\(self.hungerpok1value)"
+//        self.healthpok1.text = "\(self.healthpok1value)"
+//                   self.healthpok2.text = "\(self.healthpok2value)"
+//                   self.hungrypok2.text = "\(self.hungerpok2value)"
+//
+//    }
+   
     
     
 }
